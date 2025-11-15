aa6993f63f43b1d93002feaedd8e252784585e03
Ixollozi <mirzaezmichka@gmail.com>
2025-11-16 01:39:56 +0500

    deploy

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -88,8 +88,12 @@ ASGI_APPLICATION = "core.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "ardex_new.db",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "ardex"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
--- Dockerfile
+++ Dockerfile
@@ -12,10 +12,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-ENV DJANGO_SETTINGS_MODULE=core.settings
+RUN python manage.py collectstatic --noinput
 
 CMD ["gunicorn", "core.wsgi:application", "--bind", "0.0.0.0:8000"]
-
-
-
-
